04fb4f3ccb2a930049807565c20797de4ab7005b
VINI <hubner.vini@gmail.com>
2024-11-01 23:31:49 -0300

    Criando servidor Express

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const promotionsRouter = require('./routes/promotions');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('./api/promotions', promotionsRouter);
+
+app.listen(PORT, () => {
+    console.log('Servidor rodando na porta ${PORT}')
+});
